4429135a51193932e0d7a60fb2cc643b80c774db
Henrique-TO <144277732+Henrique-TO@users.noreply.github.com>
2023-10-05 16:52:01 -0300

    Add files via upload

---
+++ aula12.py
@@ -0,0 +1,30 @@
+# x = {1, 2, 3, 4}
+# d = {3, 4, 5, 6}
+
+# print(x.difference(d))
+# print(d.difference(x))
+
+# frutas= {'maçã','pera'}
+# frutas.add('uva')
+
+# frutas.remove('maçã')
+# frutas.discard('pera')
+
+# if 'banana' in frutas:
+#     print('Banana está na lista de frutas.')
+
+# print(frutas)
+
+c1 = {1, 2, 3}
+c2 = {3, 4, 5}
+
+uniao = c1.union(c2) 
+
+
+intersecao = c1.intersection(c2)  
+
+
+diferenca = c1.difference(c2) 
+
+for c1 in c2:
+    print(c2)
